24ad1951ed80e237db9e66ee0794555f63ae7ca3
Jimmy <ableton2@centrum.cz>
2015-11-14 14:36:24 +0100

    update

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -7,8 +7,8 @@ int main()
 	using namespace sparky;
 	using namespace graphics;
 
-	Window window("Sparky!", 800, 600); 
-	glClearColor(0.2f, 0.3f, 0.8f, 23.0f);
+	Window window("Sparky!", 960, 540); 
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
@@ -16,6 +16,7 @@ int main()
 	{
 		std::cout << window.getWidth() << " " << window.getHeight() << std::endl;
 		window.clear();
+
 		glBegin(GL_TRIANGLES);
 		
 		glVertex2f(-0.5f,-0.5f);
@@ -23,17 +24,14 @@ int main()
 		glVertex2f(0.5f,-0.5f);
 		glEnd();
 		window.update();
-		
-
 	}
 
-	while(!window.closed()){
+	/*while(!window.closed()){
 		
 		window.clear();
 		window.update();
-	} 
+	} */
 	
-
 	return 0;
-		 
+
 }
